8e35e709c28acdaa3fdb30586bb2ed88c379c3db
yxd1 <yxd1@meitu.com>
2018-10-28 10:02:57 +0800

    update

--- pip_install.sh
+++ pip_install.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-apk add libevent
-
-pip_list="werkzeug flask flask_script flask_migrate gevent"
-
-for var in list; do
-    pip install -i https://pypi.tuna.tsinghua.edu.cn/simple $var
-done
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple werkzeug flask flask_script flask_migrate gevent
